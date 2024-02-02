0c1378a480d43a1f15655acc38bcc0f1e0b72045
1009464 <1009464@g.mvusd.net>
2024-02-02 11:25:24 -0800

    Declared deque and added three elements

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Deque<String> deck = new ArrayDeque<String>();
+        deck.add("Heart");
+        deck.add("Club");
+        deck.add("Diamond");
     }
 }
